67fa8c1d4a5f4401ba4be08ec65faa6c9d595642
mitikoman2017-9 <shervinpardaz2016@gmail.com>
2018-03-03 17:38:33 +0330

    12-12-96

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -48,7 +48,7 @@
 
 <div id="detail-prod" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="productGrid" dir="rtl">
     <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
+        <div class="modal-content" >
             <div class="modal-header">
                 <button type="button" class="close " data-dismiss="modal" aria-label="بستن"><span
                             aria-hidden="true">×</span></button>
